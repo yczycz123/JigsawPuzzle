955f749d2693299e1d6c7deeb45f8a3a4eda0d9a
ycz <ycz_20011201@163.com>
2024-03-26 16:29:35 +0800

    2024/3/26

--- src/com/ycz/ui/Gameif.java
+++ src/com/ycz/ui/Gameif.java
@@ -5,17 +5,15 @@ import javax.swing.*;
 public class Gameif extends JFrame {
     public Gameif(){
 
-        //设置界面的宽高
-        this.setSize(603,680);
-        //设置界面的标题
-        this.setTitle("拼图单机版");
-        //设置界面置顶，即始终显示在桌面的最前面，不会被盖住
-        this.setAlwaysOnTop(true);
-        //设置界面居中
-        this.setLocationRelativeTo(null);
-        //设置游戏关闭模式
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //3即关闭游戏同时关闭虚拟机，2是关闭所有界面时才关闭虚拟机
+        init();
 
+        initjmenu();
+
+        //设置界面可见，一般放最后
+        this.setVisible(true);
+    }
+
+    public void initjmenu() {
         //创建菜单对象
         JMenuBar jMenuBar=new JMenuBar();
 
@@ -48,8 +46,18 @@ public class Gameif extends JFrame {
 
         //给整个界面设置菜单
         this.setJMenuBar(jMenuBar);
+    }
 
-        //设置界面可见，一般放最后
-        this.setVisible(true);
+    public void init() {
+        //设置界面的宽高
+        this.setSize(603,680);
+        //设置界面的标题
+        this.setTitle("拼图单机版");
+        //设置界面置顶，即始终显示在桌面的最前面，不会被盖住
+        this.setAlwaysOnTop(true);
+        //设置界面居中
+        this.setLocationRelativeTo(null);
+        //设置游戏关闭模式
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //3即关闭游戏同时关闭虚拟机，2是关闭所有界面时才关闭虚拟机
     }
 }
